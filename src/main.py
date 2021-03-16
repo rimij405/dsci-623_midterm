c49f881511afdcbaedaafbe9eb5835e0762ad6da
Ian Effendi <effendiian@gmail.com>
2021-03-16 14:21:29 -0400

    Update repository files with environ testing info.
    VSCode specifically does not play nice with PYTHONPATH.
    
    Changelist
    - Import sys and os for checking working directory.

--- src/main.py
+++ src/main.py
@@ -1,6 +1,12 @@
+import os
+import sys
 import pandas as pd
 
 if __name__ == "__main__":
+    print(sys.executable)
+    print(sys.version)
+    print(sys.version_info)
+    print(os.listdir())
     print("Test main entry point.")
-    df = pd.read_csv("../data/mfi/mortality/mortality_long.tsv", sep="\t")
+    df = pd.read_csv("./data/country_codes.tsv", sep="\t")
     print(df)
